74195850f258bea617616295607656923488249a
jesuSando <correoepicgames9@gmail.com>
2026-08-05 23:29:26 -0400

    feat: se agregan teclas para mas comodidad

--- player.gd
+++ player.gd
@@ -11,14 +11,14 @@ const JUMP_VELOCITY = 6
 var xform : Transform3D
 
 func _physics_process(delta: float) -> void:
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var move_direction = get_move_direction(input_dir)
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		$SoundJump.play()
 		velocity.y = JUMP_VELOCITY
 		play_anim("jump")
