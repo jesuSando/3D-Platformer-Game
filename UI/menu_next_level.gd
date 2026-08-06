5cac47f6d32fe32d605bad7c5f90919cf9353fe5
jesuSando <correoepicgames9@gmail.com>
2026-08-05 21:41:05 -0400

    fix: se arregla error en la musica de la pantalla de carga

--- UI/menu_next_level.gd
+++ UI/menu_next_level.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready() -> void:
+	SoundManager.stop_level_music()
+	SoundManager.play_menu_music()
+	
 func _on_button_pressed() -> void:
 	SoundManager.stop_menu_music()
 	SoundManager.play_button_sound()
